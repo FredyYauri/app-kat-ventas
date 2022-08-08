a252cc503c172c88da11bc451b6ad0b7a5ad65fd
fyauriba <fredy12_3@hotmail.com>
2022-08-08 15:57:51 -0500

    Opciones de menú

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ClientComponent } from './client/client.component';
+import { DetailClientComponent } from './client/detail-client/detail-client.component';
+import { OrderDetailComponent } from './order/order-detail/order-detail.component';
+import { OrderComponent } from './order/order.component';
+import { ProductDetailComponent } from './product/product-detail/product-detail.component';
+import { ProductComponent } from './product/product.component';
+import { SaleComponent } from './sale/sale.component';
 
-const routes: Routes = [];
+const routes = [
+  {path: '', redirectTo: '/client', pathMatch: 'full'},
+  {path: 'client', component: ClientComponent},
+  {path: 'client-detail', component: DetailClientComponent},
+  {path: 'order', component: OrderComponent},
+  {path: 'order-detail', component: OrderDetailComponent},
+  {path: 'product', component: ProductComponent},
+  {path: 'product-detail', component: ProductDetailComponent},
+  {path: 'sale', component: SaleComponent}
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,27 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ClientComponent } from './client/client.component';
+import { RouterModule } from '@angular/router';
+import { DetailClientComponent } from './client/detail-client/detail-client.component';
+import { ProductComponent } from './product/product.component';
+import { OrderComponent } from './order/order.component';
+import { SaleComponent } from './sale/sale.component';
+import { ProductDetailComponent } from './product/product-detail/product-detail.component';
+import { OrderDetailComponent } from './order/order-detail/order-detail.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClientComponent,
+    DetailClientComponent,
+    ProductComponent,
+    OrderComponent,
+    SaleComponent,
+    ProductDetailComponent,
+    OrderDetailComponent
   ],
   imports: [
     BrowserModule,
